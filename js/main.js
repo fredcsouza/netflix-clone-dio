de1e450c8b623ec888acac9b6b5da6b81c2d8475
Frederico <fredcsouza@gmail.com>
2021-12-14 10:57:22 -0300

    trocando carrosel para o glide

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-const inicializarOwl = () => {
-    return $('.owl-carousel').owlCarousel({
-        center: true,
-        dots: false,
-        loop: true,
-        margin: 10,
-        nav: true,
-        autoWidth: true,
-        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 3
-            },
-            1000: {
-                items: 5
-            }
-        }
-    })
-}
-
-const criarTituloPrincipal = data => {
+const createMainTitle = data => {
     $('.filme-principal .titulo').text(data.name || data.title || data.original_name)
     $('.filme-principal .descricao').text(data.overview);
     $('.filme-principal').css({
@@ -30,37 +7,80 @@ const criarTituloPrincipal = data => {
     });
 }
 
-const carregarCarrosel = (name, data) => {
-    const carrosel = $('<div class="owl-carousel owl-theme"></div>')
+const createCarousel = (name, data) => {
+    let list = ''
 
     data.forEach(filme => {
         if (!filme.backdrop_path) return;
-        console.log(filme)
-        carrosel.append(`
-                <div class="item">
-                    <img src="${getUrlImage(filme.poster_path,'w200')}" alt="" class="box-filme">
-                </div>
-                `)
+
+        list = `
+                    ${list}
+                    <li class="glide__slide">
+                        <div class="item">
+                            <img src="${getUrlImage(filme.poster_path,'w200')}" alt="" class="box-filme">
+                        </div>
+                    </li>
+                    `
     });
-    const title = $(`<h2 class=genero-titulo>${name}</h2>`)
-    $('.carrosel-filmes').append(title)
-    $('.carrosel-filmes').append(carrosel)
+
+    return `
+    <h2 class=genero-titulo>${name}</h2>
+
+    <div class="glide">
+        <div class="glide__track" data-glide-el="track">
+            <ul class="glide__slides">
+                ${list}
+            </ul>
+        </div>
+        <div class="glide__arrows" data-glide-el="controls">
+            <button class="glide__arrow glide__arrow--left" data-glide-dir="<"><span><</span></button>
+            <button class="glide__arrow glide__arrow--right" data-glide-dir=">"><span>></span></button>
+        </div>
+    </div>
+    `
 }
+
+
+const loadCarrousels = (dados) => {
+    dados.map(genre => {
+        $('.wrapper-carousels').append(createCarousel(genre.genre, genre.data.results))
+    })
+
+    const glideConfig = {
+        type: 'carousel',
+        startAt: 0,
+        perView: 9,
+        gap: 12,
+        breakpoints: {
+            1000: {
+                perView: 7,
+            },
+            800: {
+                perView: 5,
+            },
+            600: {
+                perView: 3,
+            },
+            400: {
+                perView: 1,
+            }
+        }
+    }
+
+    document.querySelectorAll('.glide').forEach(item => {
+        new Glide(item, glideConfig).mount()
+    })
+}
+
 
 $(async function() {
     try {
         const result = await getDados();
         const dados = await result.json()
 
-        criarTituloPrincipal(dados[0].data.results[0])
-
-        dados.map(genre => carregarCarrosel(genre.genre, genre.data.results))
-
-        inicializarOwl()
-
+        createMainTitle(dados[0].data.results[0])
+        loadCarrousels(dados)
     } catch (error) {
         console.log(error)
     }
-
-    window.dispatchEvent(new Event('resize')); //refresh no carrosel
 })
